18163768f17d07eb41031f1f99d41f2c31775f04
navybook <kuma_d90@yahoo.co.jp>
2024-04-19 01:09:08 +0900

    ヘルプ・プライバシーポリシー等実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'help', to: 'static_pages#help'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
+  get 'terms_of_service', to: 'static_pages#terms_of_service'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :password_resets, only: %i[new create edit update]
   get 'tops/index'
@@ -26,7 +29,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   root 'tops#index'
