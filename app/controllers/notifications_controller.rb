86d735ef8a587200dee442d205f061d580a5b1e4
navybook <kuma_d90@yahoo.co.jp>
2024-10-14 18:30:58 +0900

    Notificationアクション追加

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,11 +1,42 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.notifications.order(created_at: :desc)
-    render partial: 'notifications/list', locals: { notifications: @notifications }
+    partner = current_user.emotion_partner&.partner
+
+    if partner.nil?
+      @error = 'partnerがいません'
+      render json: {
+        error: @error,
+        notifications: render_to_string(partial: 'notifications/list', locals: { notifications: [] })
+      }
+    else
+      @notifications = partner.notifications.order(created_at: :desc).limit(20)
+      # 未読通知を既読に更新する
+      @notifications.unread.update_all(status: :read)
+
+      render json: {
+        notifications: render_to_string(partial: 'notifications/list', locals: { notifications: @notifications })
+      }
+    end
   end
 
-  def show
-    @notification = Notification.find(params[:id])
-    @notification.update(status: :read) if @notification.status == 'unread'
+  def unread_count
+    partner = current_user.emotion_partner&.partner
+
+    if partner.nil?
+      render json: { unread_count: 0 }
+    else
+      unread_count = partner.notifications.unread.count  # 未読通知の数
+      render json: { unread_count: unread_count }  # JSONで返す
+    end
+  end
+
+  def destroy
+    notification = Notification.find(params[:id])
+    notification.destroy
+
+    # 通知削除後、Turbo Streamを使用して対象の通知を削除する
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(notification) }
+    end
   end
 end
