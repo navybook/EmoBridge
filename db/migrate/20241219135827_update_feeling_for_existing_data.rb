77be69140867795cf26a960e8065c8f2b9890197
navybook <kuma_d90@yahoo.co.jp>
2024-12-24 00:08:02 +0900

    構文（見た目）修正

--- db/migrate/20241219135827_update_feeling_for_existing_data.rb
+++ db/migrate/20241219135827_update_feeling_for_existing_data.rb
@@ -1,11 +1,11 @@
 class UpdateFeelingForExistingData < ActiveRecord::Migration[7.1]
   def up
-      Emotion.where(feeling: 'exhausted').update_all(feeling: 'tired')
-      Emotion.where(feeling: 'smile').update_all(feeling: 'happy')
+    Emotion.where(feeling: 'exhausted').update_all(feeling: 'tired')
+    Emotion.where(feeling: 'smile').update_all(feeling: 'happy')
   end
 
   def down
-      Emotion.where(feeling: 'tired').update_all(feeling: 'exhausted')
-      Emotion.where(feeling: 'happy').update_all(feeling: 'smile')
+    Emotion.where(feeling: 'tired').update_all(feeling: 'exhausted')
+    Emotion.where(feeling: 'happy').update_all(feeling: 'smile')
   end
 end
